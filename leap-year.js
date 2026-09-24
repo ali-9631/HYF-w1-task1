3335e62fdc57a87e7372178604a91990d135bfda
Ali <ali.m.ali.sy@gmail.com>
2026-09-24 20:22:55 +0200

    Done

--- leap-year.js
+++ leap-year.js
@@ -1,6 +1,20 @@
 import promptSync from 'prompt-sync';
 const prompt = promptSync();
 
+while (true) {
+    let year = Number(prompt("Enter a year: "));
+
+    while (Number.isNaN(year) || year < 1 || year > 9999) {
+        console.log("Please enter a valid year.");
+        year = Number(prompt("Enter a year: "));
+    }
+
+    if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) {
+        console.log(year + " is a leap year.");
+    } else {
+        console.log(year + " is not a leap year.");
+    }
+}
 
 // Write your code here
 // Guidance:
